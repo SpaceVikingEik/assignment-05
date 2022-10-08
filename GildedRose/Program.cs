86c6f16991dfe29c058f070f678c4c3a8cf34b32
eikbo <eikb@itu.dk>
2022-10-08 17:51:34 +0200

    .

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -19,19 +19,19 @@ namespace GildedRose
                 new NormalItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
                 new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
                 new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
-                new Ticket
+                new TicketItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Ticket
+                new TicketItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
                 },
-                new Ticket
+                new TicketItem
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
