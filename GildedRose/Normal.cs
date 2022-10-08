47695f3d85b46da3f9849f92a82d79c5d380fd75
eikbo <eikb@itu.dk>
2022-10-08 13:31:27 +0200

    .

--- GildedRose/Normal.cs
+++ GildedRose/Normal.cs
@@ -4,10 +4,11 @@ public class Normal : Item
     {
 
         Quality = Quality - 1;
+
         SellIn = SellIn - 1;
+        
         if (SellIn <= 0) Quality = Quality - 1;
         if (Quality < 0) Quality = 0;
-        Console.WriteLine("UWU");
 
     }
 }
